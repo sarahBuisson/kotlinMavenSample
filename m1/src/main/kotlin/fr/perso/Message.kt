ca21807207f34205994cc6f54f7fa2d79f37076b
sbuisson <sbuisson@xebia.fr>
2018-04-15 15:47:46 +0200

    init

---
+++ m1/src/main/kotlin/fr/perso/Message.kt
@@ -0,0 +1,3 @@
+package fr.perso.m1
+
+data class Message(val content:String){}
---
+++ m2/src/main/kotlin/fr/perso/Hello.kt
@@ -0,0 +1,8 @@
+package fr.perso.m2
+
+import fr.perso.m1.Message
+
+fun main(args: Array<String>) {
+    println(Message("Hello").content)
+}
+
